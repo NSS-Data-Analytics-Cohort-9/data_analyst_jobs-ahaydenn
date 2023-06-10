fcfc7b547d2f42a85b159fb4c91d5fd1154cf063
ahaydenn <ashayden918@gmail.com>
2023-06-10 10:00:53 -0500

    data analyst jobs sql complete

--- sql_scripts/completed_data_analyst_jobs_project.sql
+++ sql_scripts/completed_data_analyst_jobs_project.sql
@@ -1,15 +1,15 @@
 -- 1. How many rows are in the data_analyst_jobs table?
 
--- SELECT COUNT (*)
--- FROM data_analyst_jobs;
+SELECT COUNT (*)
+FROM data_analyst_jobs;
 
 -- Answer: 1793
 
 -- 2. Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
 
--- SELECT *
--- FROM data_analyst_jobs
--- LIMIT 10;
+SELECT *
+FROM data_analyst_jobs
+LIMIT 10;
 
 -- Answer: ExxonMobil
 
@@ -19,139 +19,113 @@ SELECT COUNT(*) AS tn
 FROM data_analyst_jobs
 WHERE location = 'TN';
 
--- SELECT COUNT(*)
--- FROM data_analyst_jobs
--- WHERE location = 'KY';
+-- Answer: 21
 
--- Answer: 21,6
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location IN ('TN','KY');
+
+-- Answer: 27
 
 -- 4.	How many postings in Tennessee have a star rating above 4?
 
--- SELECT COUNT(*)
--- FROM data_analyst_jobs
--- WHERE location = 'TN'
--- AND star_rating = '4';
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location = 'TN'
+AND star_rating > '4';
 
--- Answer: 1
+-- Answer: 3
 
 -- 5.	How many postings in the dataset have a review count between 500 and 1000?
 
--- SELECT COUNT(*)
--- FROM data_analyst_jobs
--- WHERE review_count BETWEEN 500 AND 1000;
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE review_count BETWEEN 500 AND 1000;
 
 -- Answer: 151
 
 -- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 
--- SELECT location AS state,AVG(star_rating) AS avg_rating
--- FROM data_analyst_jobs
--- WHERE star_rating IS NOT NULL
--- GROUP BY location
--- ORDER BY avg_rating DESC;
+SELECT location AS state,AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+GROUP BY location
+ORDER BY avg_rating DESC;
 
--- Answer: Nebraska
+-- Answer: Nebraska (NE)
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
--- SELECT DISTINCT title
--- FROM data_analyst_jobs;
+SELECT DISTINCT title
+FROM data_analyst_jobs;
 
--- SELECT COUNT (DISTINCT title) as title_unique
--- FROM data_analyst_jobs;
+SELECT COUNT (DISTINCT title) as title_unique
+FROM data_analyst_jobs;
 
 -- Answer: 881 
 
 -- 8.	How many unique job titles are there for California companies?
 
--- SELECT COUNT (DISTINCT title)
--- FROM data_analyst_jobs
--- WHERE location = 'CA';
+SELECT COUNT (DISTINCT title)
+FROM data_analyst_jobs
+WHERE location = 'CA';
 
 -- Answer: 230
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more than 5000 reviews across all locations?
 
--- SELECT company,AVG(star_rating) AS avg_company_rating
--- FROM data_analyst_jobs
--- WHERE star_rating IS NOT NULL
--- AND company IS NOT NULL
--- AND review_count > 5000
--- GROUP BY company;
+SELECT company, AVG(star_rating)
+FROM data_analyst_jobs
+WHERE review_count > 5000
+AND company IS NOT NULL
+GROUP BY company;
 
--- SELECT COUNT(company)
--- FROM data_analyst_jobs
--- WHERE review_count > 5000;
-
--- Answer: 184
+-- Answer: 40
 
 -- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
--- SELECT company,AVG(star_rating) AS avg_company_rating
--- FROM data_analyst_jobs
--- WHERE star_rating IS NOT NULL
--- AND company IS NOT NULL
--- AND review_count > 5000
--- GROUP BY company
--- ORDER BY avg_company_rating DESC
--- LIMIT 1;
+SELECT company,AVG(star_rating) AS avg_company_rating
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+AND company IS NOT NULL
+AND review_count > 5000
+GROUP BY company
+ORDER BY avg_company_rating DESC
+LIMIT 10;
 
--- Answer: Microsoft 4.2 rating (rounded up)
+-- Answer:6 way tie between General Motors, Microsoft, Kaiser Permanente, Unilever, Nike, American Express
 
 -- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
 
--- SELECT title
--- FROM data_analyst_jobs
--- WHERE title LIKE '%Analyst%';
+SELECT DISTINCT (title)
+FROM data_analyst_jobs
+WHERE title iLIKE '%Analyst%';
 
--- SELECT COUNT(title)
--- FROM data_analyst_jobs
--- WHERE title LIKE '%Analyst%';
-
--- Answer: 1636
+-- Answer: 774
 
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
--- SELECT COUNT(title)
--- FROM data_analyst_jobs
--- WHERE title NOT LIKE '%Analyst%'
--- AND title NOT LIKE '%Analytics%';
 
--- SELECT title
--- FROM data_analyst_jobs
--- WHERE title NOT LIKE '%Analyst%'
--- AND title NOT LIKE '%Analytics%';
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT iLIKE '%Analyst%'
+AND title NOT iLIKE '%Analytics%';
 
--- Answer: 39. These job titles are all upper or lower cased.
+-- Answer: 4-Tableau
 
 -- ##BONUS## You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
 -- Disregard any postings where the domain is NULL. 
 -- Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
 -- Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4? 
 
--- SELECT domain, COUNT(title) as sql_title
--- FROM data_analyst_jobs
--- WHERE skill LIKE '%SQL%'
--- AND days_since_posting > 21
--- AND domain IS NOT NULL
--- GROUP BY domain;
-
--- SELECT domain, COUNT(title) as sql_title
--- FROM data_analyst_jobs
--- WHERE skill LIKE '%SQL%'
--- AND days_since_posting > 21
--- AND domain IS NOT NULL
--- GROUP BY domain
--- ORDER BY sql_title DESC;
-
--- SELECT domain, COUNT(title) as sql_title
--- FROM data_analyst_jobs
--- WHERE skill LIKE '%SQL%'
--- AND days_since_posting > 21
--- AND domain IS NOT NULL
--- GROUP BY domain
--- ORDER BY sql_title DESC
--- LIMIT 4;
+SELECT domain, COUNT(title) as sql_title
+FROM data_analyst_jobs
+WHERE skill LIKE '%SQL%'
+AND days_since_posting > 21
+AND domain IS NOT NULL
+GROUP BY domain
+ORDER BY sql_title DESC
+LIMIT 4;
 
 -- Answer: 
 -- Top 4 industries are Internet and Software, Banks and Financial Services, Consulting and Business Services, and Health Care.
